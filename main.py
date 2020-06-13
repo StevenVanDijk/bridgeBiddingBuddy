f3457beb4f90c64d915279bc88bf137dd9dc5f0a
Marieke van der Valk <marieke@summadigita.com>
2020-06-13 16:12:50 +0200

    Toenemende biedingen

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ from kivy.uix.anchorlayout import AnchorLayout
 class BridgeBiddingBuddy(App):
     currentBidding = None
     defaultHeight = 50
+    value = 0
+    lastBid = ''
+    buttonsDict = {
+        '1♣': 1, '2♣': 5, '3♣': 9, '4♣': 13, '5♣': 17, '6♣': 21, '7♣': 25,
+        '1♦': 2, '2♦': 6, '3♦': 10, '4♦': 14, '5♦': 18, '6♦': 22, '7♦': 26,
+        '1♥': 3, '2♥': 7, '3♥': 11, '4♥': 15, '5♥': 19, '6♥': 23, '7♥': 27,
+        '1♠': 4, '2♠': 8, '3♠': 12, '4♠': 16, '5♠': 20, '6♠': 24, '7♠': 28  
+    }
 
     def buildLabel(self, txt):
         return Label(text=txt, size_hint=(1.0, None),  height=self.defaultHeight)
 
-    def onAddBid(self, value):
-        self.currentBidding.add_widget(self.buildLabel(value))
+    def onAddBid(self, bid):
+        if bid > self.lastBid:
+            self.lastBid = bid
+            self.currentBidding.add_widget(self.buildLabel(bid))
 
     def build(self):
         rootLayout = BoxLayout(orientation='horizontal')
@@ -37,6 +47,7 @@ class BridgeBiddingBuddy(App):
                 def createCallback(elem):
                     return lambda instance: self.onAddBid(elem)
                 AButton.bind(on_press=createCallback(elem))
+                self.lastBid = elem
                 result.add_widget(AButton)
             return result            
 
@@ -64,6 +75,8 @@ class BridgeBiddingBuddy(App):
         rootLayout.add_widget(rightLayout)
 
         return rootLayout
+
+            
 
 if __name__ == '__main__':
     app = BridgeBiddingBuddy()

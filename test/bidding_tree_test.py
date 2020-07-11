740b9e0efb062e30b449dda13bd4b18db99a28ae
Steven van Dijk <zteven@gmail.com>
2020-07-11 14:42:08 +0200

    make tests parametrized

--- test/bidding_tree_test.py
+++ test/bidding_tree_test.py
@@ -1,141 +1,28 @@
 from bidding_tree import bidding_tree
+import pytest
 
-def testYouOpen():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 12
-    highest = 5
-    lowest = 0 
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == "1♥"
+testcases = [
+    ("you open", [], 12, 5, 0, 'Hs', "1Hs"),
+    ("you open pass", [], 11, 6, 0, 'Ss', "pass"),
+    ("you open preemtif", [], 8, 7, 0, 'Ss', '3Ss'),
+    ("you open 1SA", [], 16, 5, 2, 'Ss', '1SA'),
+    ("you open 1Cs", [], 13, 4, 0, 'Ss', '1Cs'),
+    ("you open 2SA", [], 21, 5, 2, 'Hs', '2SA'),
+    ("you open 2Cs w pts", [], 25, 0, 0, 'Hs', '2Cs'),
+    ("you open 2Cs", [], 16, 7, 0, 'Hs', '2Cs'),
+    ("you open 2Hs", [], 0, 6, 0, 'Hs', '2Hs'),
+    ("you open 2SA", [], 21, 5, 3, 'Hs', '2SA'),
+    ("jacoby", ["1SA", "pass"], 0, 5, 0, 'Hs', '2Ds'),
+    ("stayman", ["1SA", "pass"], 8, 4, 0, 'Hs', '2Cs'),
+    ("answer 2SA to 1SA", ["1SA", "pass"], 8, 4, 0, 'Cs', '2SA'),
+    ("answer 3SA to 1SA", ["1SA", "pass"], 10, 4, 0, 'Cs', '3SA'),
+    ("answer to partner lvl 1", ["1Cs", "pass"], 90, 4, 0, 'Hs', '1Hs')
+]
 
-def testYouOpenPass():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 11
-    highest = 6
-    lowest = 0
-    color = '♠'
-    assert bt.bids(cb, points, highest, lowest, color) == 'pass'
-
-def testYouOpenPreemtif():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 8
-    highest = 7
-    lowest = 0
-    color = '♠'
-    assert bt.bids(cb, points, highest, lowest, color) == '3♠'
-
-def testYouOpen1SA():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 16
-    highest = 5
-    lowest = 2
-    color = '♠'
-    assert bt.bids(cb, points, highest, lowest, color) == '1SA'
-
-def testYouOpen1clubs():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 13
-    highest = 4
-    lowest = 0
-    color = '♠'
-    assert bt.bids(cb, points, highest, lowest, color) == '1♣'
-
-def testYouOpen2SA():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 21
-    highest = 5
-    lowest = 2
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2SA'
-
-def testYouOpen2clubsWithPoints():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 25
-    highest = 0
-    lowest = 0
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2♣'
-
-def testYouOpen2clubs():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 16
-    highest = 7
-    lowest = 0
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2♣'
-
-def testYouOpen2hearts():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 0
-    highest = 6
-    lowest = 0
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2♥'
-
-def testYouOpen2SA():
-    cb = []
-    bt = bidding_tree(cb)
-    points = 21
-    highest = 5
-    lowest = 3
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2SA'
-
-def testJacoby():
-    cb = ['1SA', 'pass']
-    bt = bidding_tree(cb)
-    points = 0
-    highest = 5
-    lowest = 0
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2♦'
-
-def testStayman():
-    cb = ['1SA', 'pass']
-    bt = bidding_tree(cb)
-    points = 8
-    highest = 4
-    lowest = 0
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '2♣'
-
-def testAnswer2SATo1SA():
-    cb = ['1SA', 'pass']
-    bt = bidding_tree(cb)
-    points = 8
-    highest = 4
-    lowest = 0
-    color = '♣'
-    assert bt.bids(cb, points, highest, lowest, color) == '2SA'
-
-def testAnswer3SATo1SA():
-    cb = ['1SA', 'pass']
-    bt = bidding_tree(cb)
-    points = 10
-    highest = 4
-    lowest = 0
-    color = '♣'
-    assert bt.bids(cb, points, highest, lowest, color) == '3SA'
-
-def testAnswerToPartnerAtLevel1():
-    cb = ['1♣', 'pass']
-    bt = bidding_tree(cb)
-    points = 90
-    highest = 4
-    lowest = 0
-    color = '♥'
-    assert bt.bids(cb, points, highest, lowest, color) == '1♥'
-
-
-
-
-    
+@pytest.mark.parametrize('descr, cb, points, highest, lowest, color, expected', testcases)
+def testBidding(descr, cb, points, highest, lowest, color, expected):
+    def pretty(s):
+        return s.replace("Hs", "♥").replace("Ss", "♠").replace("Ds", "♦").replace("Cs", "♣")
+    current = [pretty(b) for b in cb]
+    bt = bidding_tree(current)
+    assert bt.bids(current, points, highest, lowest, pretty(color)) == pretty(expected)

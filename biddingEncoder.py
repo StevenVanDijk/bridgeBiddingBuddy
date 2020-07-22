bb5e652619d052bc60a394ca7c36e17879bb6ed5
Steven van Dijk <zteven@gmail.com>
2020-07-22 16:41:59 +0200

    Finish serialization bidding<->Json

--- biddingEncoder.py
+++ biddingEncoder.py
@@ -6,6 +6,7 @@ class BiddingEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Bidding):
             result = {
+                "__type__": "Bidding",
                 "current": obj.current,
                 "whoStarts": obj.whoStarts,
                 "nrOfPoints": obj.nrOfPoints,
@@ -14,3 +15,20 @@ class BiddingEncoder(json.JSONEncoder):
             return result
         # Let the base class default method raise the TypeError
         return json.JSONEncoder.default(self, obj)
+
+def bidToJson(bid: Bidding) -> str:
+    return json.dumps(bid, cls=BiddingEncoder, ensure_ascii=False)
+
+def json2Bid(jsonStr: str) -> Bidding:
+    def bidBuilder(dictionary: dict) -> Bidding:
+        if "__type__" in dictionary and dictionary["__type__"] == "Bidding":
+            result = Bidding()
+            result.current = dictionary["current"]
+            result.whoStarts = dictionary["whoStarts"]
+            result.nrOfPoints = dictionary["nrOfPoints"]
+            result.nrOfCards = dictionary["nrOfCards"]
+            return result
+        else:
+            return dictionary
+
+    return json.loads(jsonStr, object_hook=bidBuilder)
